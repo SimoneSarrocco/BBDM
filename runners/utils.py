8592c181db492777920f439959c0e92500fb7aa4
simone.sarrocco <simone.sarrocco@studio.unibo.it>
2025-04-02 15:43:59 +0000

    removed the .add_(0.5) when converting the image grid back to [0,255]

--- runners/utils.py
+++ runners/utils.py
@@ -80,5 +80,6 @@ def get_image_grid(batch, grid_size=4, to_normal=True):
     image_grid = make_grid(batch, nrow=grid_size)
     if to_normal:
         image_grid = image_grid.mul_(0.5).add_(0.5).clamp_(0, 1.)
-    image_grid = image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+    # image_grid = image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+    image_grid = image_grid.mul_(255).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
     return image_grid
